a959bfab7eca0b066864f1878c1a95ec7b3acb23
CattenLinger <cattenlinger@outlook.com>
2019-05-05 16:35:15 +0800

    More friendly login error

--- src/main/java/com/ordersys/controller/error/ErrorAdviser.java
+++ src/main/java/com/ordersys/controller/error/ErrorAdviser.java
@@ -2,6 +2,8 @@ package com.ordersys.controller.error;
 
 import com.ordersys.commons.BusinessException;
 import com.ordersys.commons.RexModel;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,4 +18,15 @@ public class ErrorAdviser {
         return new RexModel().withMessage(e.getMessage()).withError(e.getError());
     }
 
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public RexModel authenticationException(AuthenticationException e) {
+        return new RexModel().withMessage(e.getMessage()).withError("authentication_exception");
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public RexModel authorizationException(AuthorizationException e) {
+        return new RexModel().withMessage(e.getMessage()).withError("authorization_exception");
+    }
 }
