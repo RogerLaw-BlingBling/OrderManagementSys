afe99a463b3cbc52558dab0e62d4b69ad4e3876d
RogerLaw <893049558@qq.com>
2019-05-05 18:03:24 +0800

    modified:   pom.xml 	modified:   src/main/java/com/ordersys/Application.java 	modified:   src/main/resources/application.yml

--- src/main/java/com/ordersys/Application.java
+++ src/main/java/com/ordersys/Application.java
@@ -2,9 +2,6 @@ package com.ordersys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.io.File;
 
 @SpringBootApplication
 public class Application {
