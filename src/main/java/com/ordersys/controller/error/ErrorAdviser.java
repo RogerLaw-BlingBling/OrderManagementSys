e17001621837f1520e15e8d6134d0be28fe9dd88
CattenLinger <cattenlinger@outlook.com>
2019-05-05 19:24:46 +0800

    Commit

--- src/main/java/com/ordersys/controller/error/ErrorAdviser.java
+++ src/main/java/com/ordersys/controller/error/ErrorAdviser.java
@@ -4,11 +4,9 @@ import com.ordersys.commons.BusinessException;
 import com.ordersys.commons.RexModel;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authz.AuthorizationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ErrorAdviser {
@@ -24,7 +22,6 @@ public class ErrorAdviser {
     }
 
     @ExceptionHandler(AuthorizationException.class)
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<RexModel> authorizationException(AuthorizationException e) {
         return ResponseEntity.ok(new RexModel().withMessage(e.getMessage()).withError("authorization_exception"));
     }
